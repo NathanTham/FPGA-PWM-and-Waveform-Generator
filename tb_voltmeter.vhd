7a2122fbbd043cf6c3d457fcb9b493c327dca2fc
NathanTham <48495712+NathanTham@users.noreply.github.com>
2019-11-28 15:48:01 -0700

    Add files via upload

--- tb_voltmeter.vhd
+++ tb_voltmeter.vhd
@@ -9,15 +9,25 @@ ARCHITECTURE behavior OF tb_voltmeter IS
 
     COMPONENT Voltmeter IS
     PORT(
-			clk, reset : IN std_logic;
-			S	 : in  STD_LOGIC; --controls mux	
-            S2   : in  STD_LOGIC;
-		    LEDR : OUT std_logic_vector (9 downto 0);
-		    HEX0, HEX1, HEX2, HEX3, HEX4, HEX5 : OUT std_logic_vector (7 downto 0)
+			clk                           : in  STD_LOGIC;
+			reset                         : in  STD_LOGIC;
+			S						  	  : in  STD_LOGIC; --controls mux
+			S2                         	  : in  STD_LOGIC; --controls lo or high distance
+		    s3						  	  : in  STD_LOGIC; --wavecontrol sawtooth 
+		    s4						  	  : in  STD_LOGIC; --wavecontrol triangle
+		    s5						  	  : in  STD_LOGIC; --wavecontrol square
+		    s6						  	  : in  STD_LOGIC; --frequency/amplitude select
+		    but0						  : in  STD_LOGIC; --amplitude select
+		    but1						  : in  STD_LOGIC; --amplitude select
+			PWM_OUTWAVE                   : out STD_LOGIC;
+		    PWM_BUZ_OUTWAVE				  : out STD_LOGIC; -- buzzer
+			LEDR                          : out STD_LOGIC_VECTOR (9 downto 0);
+			HEX0,HEX1,HEX2,HEX3,HEX4,HEX5 : out STD_LOGIC_VECTOR (7 downto 0)
     );
     END COMPONENT;
     
-	 signal clk,reset,S : std_logic;
+	 signal clk, reset, S, S2, s3,s4, s5, s6, but0, but1  : std_logic;
+	 signal PWM_OUTWAVE, PWM_BUZ_OUTWAVE : std_logic;
 	 signal LEDR : std_logic_vector (9 downto 0);
 	 signal HEX0, HEX1, HEX2, HEX3, HEX4, HEX5 : std_logic_vector (7 downto 0);
     
@@ -28,6 +38,14 @@ ARCHITECTURE behavior OF tb_voltmeter IS
 			reset => reset,
 			S => S,
 			S2 => '0',
+			s3 => s3,
+			s4 => s4,
+			s5 => s5,
+			s6 => s6,
+			but0 => but0,
+			but1 => but1,
+			PWM_OUTWAVE => PWM_OUTWAVE,
+			PWM_BUZ_OUTWAVE => PWM_BUZ_OUTWAVE,
 			LEDR => LEDR,
 			HEX0 => HEX0,
 			HEX1 => HEX1,
@@ -37,7 +55,8 @@ ARCHITECTURE behavior OF tb_voltmeter IS
 			HEX5 => HEX5
     );
     
-    -- Stimulus process here
+	-- Stimulus process here
+
     -- Stimulus process 
     clk_process : process
 	 constant clk_period:time:=10 ns;
@@ -47,6 +66,52 @@ ARCHITECTURE behavior OF tb_voltmeter IS
 		clk <= '1';
 		wait for clk_period/2;
 	 end process;
+
+	Wave_type : process
+constant clk_period:time:=10 ns;
+	begin
+		s3 <= '1'; --sawtooth
+		s4 <= '0';
+		s5 <= '0';
+		wait for clk_period*200;
+		s3 <= '0';
+		s4 <= '1'; --triangle
+		s5 <= '0';
+		wait for clk_period*200;
+		s3 <= '0';
+		s4 <= '0';
+		s5 <= '1'; --square
+		wait for clk_period*200;
+	end process; 
+	
+	AM_or_FM : process
+constant clk_period:time:=10 ns;
+	begin
+
+		s6 <= '1';
+		wait for clk_period*100;
+		s6 <= '0';
+		wait for clk_period*100;
+	end process;
+
+	Modulation : process
+constant clk_period:time:=10 ns;
+	begin
+
+		but0 <= '1';
+		wait for clk_period*3;
+		but0 <= '1';
+		wait for clk_period*3;
+		but0 <= '1';
+		wait for clk_period*3;
+		but1 <= '1';
+		wait for clk_period*3;
+		but1 <= '1';
+		wait for clk_period*3;
+		but1 <= '1';
+		wait for clk_period*3;
+	end process;
+
 
 	 mux_proc : process
 	 begin
@@ -67,5 +132,7 @@ ARCHITECTURE behavior OF tb_voltmeter IS
 	 reset <= '0';
 	 wait;
 	 end process;
+
+
 
 END;
